e079d11712f9be110ecaf786b2ea25dd0d5df590
MaximePag <max_p@live.fr>
2021-03-04 12:11:49 +0100

    fixs

--- JML-Traders/Scripts/script.js
+++ JML-Traders/Scripts/script.js
@@ -1,5 +1,4 @@
-﻿alert('bonk');
-    $('#deleteAppointmentModal').on('show.bs.modal', function (event) {
+﻿$('#deleteAppointmentModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var id = button.data('id') // Extract info from data-* attributes
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
